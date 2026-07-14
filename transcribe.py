58e20b3f158b272e1063c8a4755968b72ae1db51
Topoiso <theomcph@gmail.com>
2026-07-14 23:44:12 +0200

    worked on tranascibe

--- transcribe.py
+++ transcribe.py
@@ -10,17 +10,26 @@ def run_transcription_pipeline():
     model = WhisperModel(model_size, device="cuda", compute_type="float16")
     all_segments = []
 
-audio_dir = "./workspace/projects/Candlekeep/recordings/test_session"
+    audio_dir = "./workspace/projects/Candlekeep/recordings/test_session"
 
-if not os.path.exists(audio_dir):
-    os.makedirs(audio_dir, exist_ok = True)
-    print("folder not found creating new folder. Please place files in audio_dir directory")
+    if not os.path.exists(audio_dir):
+        os.makedirs(audio_dir, exist_ok = True)
+        print("folder not found creating new folder. Please place files in audio_dir directory")
 
-for file in os.listdir(audio_dir):
-    if ".flac" in file or ".wav" in file:
-       speaker_id = os.path.splitext(file)[0]
+    for file in os.listdir(audio_dir):
+        if ".flac" in file or ".wav" in file:
+            speaker_id = os.path.splitext(file)[0]
        
+            file_path = os.path.join(audio_dir, file)
+            print(f"Transcribing {file_path} on your 3060 Ti...")
 
+            segments ,info = model.transcribe(audio = file_path,language="en",)
+
+            for segment in segments:
+                all_segments.append({"start": segment.start,
+                                     "speaker": speaker_id,
+                                     "text":segment.text.strip()
+                                     })
 
 
 
